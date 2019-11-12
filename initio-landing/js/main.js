61fd41192fbbfb917052a0c8150c9e86cf96cb7c
Dmitry Kragel <dmitry.kragel@gmail.com>
2019-11-12 10:19:22 +0300

    complete JS scripts

--- initio-landing/js/main.js
+++ initio-landing/js/main.js
@@ -12,15 +12,32 @@ jQuery(document).ready(function() {
 			scrollTop:0
 		}, 800);
 		return false;
-	})
+	});
 
 	/* Чекбокс */
 	jQuery('#js-agreement').click(function() {
-		var elem = jQuery('#hidden-agreement')
+		var elem = jQuery('#hidden-agreement');
 		elem.attr('checked') ? elem.removeAttr('checked') : elem.attr('checked', 'true');
 		jQuery(this).toggleClass('checkbox--active');
-	})
+	});
 
 	/* Маска полей ввода телефона */
 	jQuery("#user_phone").mask("+7 (999) 999 99 99");
+	jQuery("#user_phone_callback").mask("+7 (999) 999 99 99");
+
+	/* открытие формы коллбэка */
+	var form = jQuery('form.callback');
+	var dark = jQuery('#js-dark');
+
+	jQuery('#js-callback').click(function() {
+		dark.fadeIn(function(){
+			form.fadeIn();
+		});
+	});
+
+	jQuery('#js-callback-close').click(function() {
+		form.fadeOut(function(){
+			dark.fadeOut();
+		});
+	})
 });
