958b97498b823633b967ea98e5521db0f373e77f
Dmitry Kragel <dmitry.kragel@gmail.com>
2019-11-13 03:01:52 +0300

    add meta info from data file

--- initio_landing/index.php
+++ initio_landing/index.php
@@ -4,7 +4,8 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Document</title>
+	<title><?= $meta_title ?></title>
+	<meta name="description" content="<?= $meta_description ?>" />
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
