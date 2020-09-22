2997649c0c39bb5e5d7219f0b94ffe6af2198d73
Dmitry Kragel <dmitry.kragel@gmail.com>
2020-09-23 00:47:21 +0300

    add icebreakers

--- ivasi/header.php
+++
@@ -1,9 +0,0 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="js/jquery-ui-1.12.1/jquery-ui.structure.min.css">
-<link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
-<link rel="stylesheet" href="css/style_recipe.css?v=<?php echo time(); ?>">
-<meta name="format-detection" content="telephone=no">
-<link rel="icon" href="favicon.ico" type="image/x-icon">
-
-
--- kelnik_test/js/common.js
+++
@@ -1,4 +0,0 @@
-/* kelnik test */
-(function () {
-
-})();
